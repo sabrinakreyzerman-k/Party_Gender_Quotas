d92a2e846f422365110c3a67a708081e61705053
Sabrina Kreyzerman <sabrina.kreyzerman@theijf.org>
2026-05-15 02:24:55 +0000

    Created final politician dataset for use

---
+++ scripts/data_cleaning/final_politician_data.R
@@ -0,0 +1,24 @@
+#### Preamble ####
+# Purpose: Create final dataset for use
+# Author: Sabrina Kreyzerman
+# Date: 11 May 2026
+# Contact: sabrina.kreyzerman@mail.utoronto.ca
+# License: MIT
+
+#### Workspace setup ####
+## Load packages ##
+library(tidyverse)
+library(here)
+library(dplyr)
+
+## Read in datasets ##
+combined_politician_data <- read_csv(here("data/clean/combined_politician_data.csv"))
+
+final_politician_data <- combined_politician_data |>
+  select(uniqueID, name, gender, partyName, year_elected)
+
+#### Save final dataset ####
+write_csv(combined_politician_data, "data/clean/final_politician_data.csv")
+
+#### Read in final dataset ####
+final_politician_data <- read_csv("data/clean/final_politician_data.csv")
